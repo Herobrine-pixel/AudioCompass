febce87d30ee887dd2f2b68456cb8df0e3ce6857
Herobrine Sah <ashasah77@gmail.com>
2025-06-11 12:29:57 +0530

    Update AudioDirectionDemo.ino

--- examples/AudioDirectionDemo/AudioDirectionDemo.ino
+++ examples/AudioDirectionDemo/AudioDirectionDemo.ino
@@ -1,1 +1,16 @@
 
+
+#include <AudioCompass.h>
+
+void setup() {
+    Serial.begin(115200);
+    AudioCompass.begin();
+}
+
+void loop() {
+    String direction = AudioCompass.getDirection();
+    Serial.println("Detected Direction: " + direction);
+    delay(1000);
+}
+
+
